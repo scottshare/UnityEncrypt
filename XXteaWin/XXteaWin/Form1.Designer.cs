b4a376d07df9faf632409759d36d9654e4f576e3
wenney_esoft@live.cn <wenney_esoft@live.cn>
2018-07-24 19:50:16 +0800

    添加主项目，解决unity托管代码的加壳

---
+++ XXteaWin/XXteaWin/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace XXteaWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(110, 116);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(167, 52);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "加密";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 295);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
+            this.Text = "加密DLL";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ XXteaWin/XXteaWin/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xxtea;
+
+namespace XXteaWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            var sd = openFileDialog1.ShowDialog();
+            if (sd == DialogResult.OK)
+            {
+                String key = "1234567890";
+                using (var fs = openFileDialog1.OpenFile())
+                {
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, buffer.Length);
+                    var data = XXTEA.Encrypt(buffer, key);
+                    var file = new FileInfo(openFileDialog1.FileName);
+                    string fname = Path.Combine(file.DirectoryName,
+                        Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + "_secure" +
+                        Path.GetExtension(openFileDialog1.FileName));
+                    if (File.Exists(fname))
+                        File.Delete(fname);
+                    using (var fs2 = File.Create(fname))
+                    {
+                        fs2.Write(data, 0, data.Length);
+                    }
+                }
+            }
+        }
+    }
+}
